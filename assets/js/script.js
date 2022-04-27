e8c53330c069fd4cfb52bcbf8f998461a448a34e
coder-swan <charlesandrewswan@gmail.com>
2022-04-27 11:12:17 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,24 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clciked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type"); 
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
-function runGame(){
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
-    
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unkown game type ${gameType}`);
+        throw `Unknown game type: ${gameType}.Aborting!`
+    }
 
 }
 
@@ -38,7 +46,11 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1'). textContent = operand1;
+    document.getElementById('operand2'). textContent = operand2;
+    document.getElementById('operator'). textContent = "+";
+
 
 }
 
