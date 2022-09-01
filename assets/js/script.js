a0996ad65c96f2b92ff2d6e2535a3bfc55d38c73
coder-swan <charlesandrewswan@gmail.com>
2022-09-01 11:59:06 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons){
+    for (let button of buttons) {
         button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit"){
+            if (this.getAttribute("data-type") === "submit") {
                 alert("You clciked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type"); 
